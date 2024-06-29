b5f2e83f49d02c674bef515d6e87832614c74ca7
Ladzi031 <ladziladzani@gmail.com>
2024-06-29 12:25:51 +0200

    first commit

---
+++ noteServer/src/main/java/com/diary/noteToSelf/NoteToSelfApplication.java
@@ -0,0 +1,12 @@
+package com.diary.noteToSelf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteToSelfApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(NoteToSelfApplication.class, args);
+	}
+
+}
---
+++ noteServer/src/test/java/com/diary/noteToSelf/NoteToSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.diary.noteToSelf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteToSelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
