c2e16d52da5b2a76d8281d87f5c458136e2c235d
Ladzi031 <ladziladzani@gmail.com>
2024-06-29 15:23:30 +0200

    layout: add register page layout

--- noteClient/src/app/app-routing.module.ts
+++ noteClient/src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
   { path: "", component: HomeComponent },
-  { path: "home", redirectTo: "", pathMatch: "full" }
+  { path: "login", component: LoginComponent },
+  { path: "register", component: RegisterComponent },
+  { path: "home", redirectTo: "", pathMatch: "full" },
+  { path: "**", redirectTo: "", pathMatch: "full" }
 ];
 
 @NgModule({
