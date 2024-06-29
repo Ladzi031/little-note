609ff31e903eb518625f8cfe836f24af5b45e933
Ladzi031 <ladziladzani@gmail.com>
2024-06-29 20:49:38 +0200

    Pages: add additional web-pages

---
+++ noteClient/src/app/pages/list-all/list-all.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-list-all',
+  templateUrl: './list-all.component.html',
+  styleUrls: ['./list-all.component.css']
+})
+export class ListAllComponent {
+
+}
